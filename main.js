24fc7d8f02cf6db829969a6f39d4f7b7feba9b9d
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-09-16 16:58:11 +0300

    still making contributions

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ console.log("I only exist to trigger workflows");
 
 console.log("Well, i made a contribution!");
 console.log("Mama would be proud");
+
+console.log("I made another one, since I messed up the workflow");
