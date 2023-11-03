1b443e1d8042d16ba9bbf1593fa27396ea4ded74
Konstantinos Kalafatis <kalafatiskwstas@gmail.com>
2023-11-03 14:34:51 +0200

    :fire: Purge previous actions

--- main.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello World!");
-console.log("I only exist to trigger workflows");
-
-console.log("Well, i made a contribution!");
-console.log("Mama would be proud");
-
-console.log("I made another one, since I messed up the workflow");
